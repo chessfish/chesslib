0cc421d18586c05d65eb7f9d05c892d127de2b8f
humanchimp <morphcham@gmail.com>
2014-08-07 14:41:41 -0700

    gulpfile: put "watch" behind a flag

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,17 +3,22 @@ var gulp = require('gulp');
 var traceur = require('gulp-traceur');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
+var argv = require('minimist')(process.argv);
 
 gulp.task('build', function (options) {
-  console.log(options);
   esify('./src/*.es');
   esify('./src/pieces/*.es');
 });
 
 function esify(src) {
   var p = path.normalize(path.dirname(src)).replace(/^src\/?/, '');
-  gulp.src(src).
-    pipe(watch()).
+  var stream = gulp.src(src);
+
+  if (argv.w || argv.watch) {
+    stream = stream.pipe(watch());
+  }
+
+  stream.
     pipe(traceur({
       experimental: true,
       sourceMaps: true

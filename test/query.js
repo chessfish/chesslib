788e48093e0a46d9756977adbccafc3bbcf63cdd
humanchimp <morphcham@gmail.com>
2014-08-07 22:49:26 -0700

    test: verify fen and query via some features of Morphy's puzzle

--- test/query.js
+++ test/query.js
@@ -9,6 +9,8 @@ var PAWN = constants.PAWN;
 var WHITE = constants.WHITE;
 var BLACK = constants.BLACK;
 var KING = constants.KING;
+var BISHOP = constants.BISHOP;
+var ROOK = constants.ROOK;
 
 var startingPosition =
 	new Fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
@@ -71,4 +73,37 @@ test('the active king in the starting position is white', function (t) {
 	});
 	t.equal(activeKing.brand, KING, 'the piece is a king');
 	t.equal(activeKing.color, WHITE, 'the piece is white');
+});
+
+var morphyPuzzle = new Fen('kbK5/pp6/1P6/8/8/8/8/R7 w - - 0 1').position
+
+test("morphy's puzzle features", function (t) {
+	t.plan(9);
+	t.equal(morphyPuzzle.queryAll().length, 7, 'there are seven pieces');
+	t.equal(morphyPuzzle.queryAll({
+		color: WHITE
+	}).length, 3, 'three of them are white');
+	t.equal(morphyPuzzle.queryAll({
+		color: BLACK
+	}).length, 4, 'four of them are black');
+	t.equal(morphyPuzzle.queryAll({
+		brand: PAWN,
+		color: BLACK
+	}).length, 2, 'two are black pawns');
+	t.equal(morphyPuzzle.queryAll({
+		brand: KING
+	}).length, 2, 'two are kings');
+	t.equal(morphyPuzzle.queryAll({
+		color: BLACK,
+		brand: BISHOP
+	}).length, 1, 'one is a black bishop');
+	t.equal(morphyPuzzle.queryAll({
+		color: WHITE,
+		brand: PAWN
+	}).length, 1, 'one is a white pawn');
+	t.equal(morphyPuzzle.queryAll({
+		color: WHITE,
+		brand: ROOK
+	}).length, 1, 'the remaining piece is a white rook');
+	t.equal(morphyPuzzle.activeColor, WHITE, 'it is white to move');
 });

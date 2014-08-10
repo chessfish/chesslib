20d5e9985f170b165a3e707c4bb5636650cef6f7
humanchimp <morphcham@gmail.com>
2014-08-10 08:48:32 -0700

    set and unset the registeredPiece and draggingPiece

--- src/ui/playable/positionview.js
+++ src/ui/playable/positionview.js
@@ -1,26 +1,37 @@
 import { h, input, event, struct, value } from 'mercury';
 
-const dragEvent = require('../drag-handler.js');
+const dragEvent = require('./drag-handler.js');
 
 const ranks = '87654321'.split('');
 const files = 'abcdefgh'.split('');
 
-export const playable = (position) => {
+export function playable(position) {
 
 	const state = struct({
 		position: value(position),
-		activePiece: value(null),
+		draggingPiece: value(null),
+		registeredPiece: value(null),
 		offset: value(null),
-		events: input(['dragPiece'])
+		targetSquare: value(null),
+		events: input(['dragPiece', 'registerPiece', 'dropPiece']),
 	});
 
 	state.events.dragPiece(({ piece, x, y }) => {
-		state.activePiece.set(piece);
+		state.draggingPiece.set(piece);
 		state.offset.set({ x, y });
 	});
 
+	state.events.registerPiece(({ piece }) => {
+		state.registeredPiece.set(piece);
+	});
+
+	state.events.dropPiece(({ piece }) => {
+		state.draggingPiece.set(null);
+		state.registeredPiece.set(null);
+	});
+
 	return state;
-};
+}
 
 playable.render = (state) =>
 	h('div.board', ranks.map((r, i) =>
@@ -32,15 +43,18 @@ function renderPiece(state, rank, file) {
 	if (piece == null) {
 		return null;
 	}
+	const options = { piece };
 	return h(`div.piece.${piece.fenEncoding}`, {
-		'ev-mousedown': dragEvent(state.events.dragPiece, { piece }),
+		'ev-mousedown': dragEvent(state.events.dragPiece, options),
+		'ev-click': event(state.events.registerPiece, options),
+		'ev-mouseup': event(state.events.dropPiece, options),
 		'style': (
-			piece === state.activePiece ? {
+			piece === state.draggingPiece ? {
 				'top': state.offset.y + 'px',
 				'left': state.offset.x + 'px',
 				'z-index': 2,
 			} : {
-				'z-index': 1
+				'z-index': 1,
 			}
 		),
 	});

0ffab0295e7b310f8f2d66ccd712d55e3cc254cc
humanchimp <morphcham@gmail.com>
2014-08-10 21:53:14 -0700

    mobility: mostly working riders

--- src/piece/trait/rider.js
+++ src/piece/trait/rider.js
@@ -1,13 +1,23 @@
-import { Mobility } from '../mobility';
+import { Point } from '../../point'
+import { Mobility, quadrants } from '../mobility';
 
 class RiderMobility extends Mobility {
-  *adjacentPoints() {
-    const { m, n } = this;
-  }
+	*adjacentPoints(p0) {
+		const { m, n } = this;
 
-  toString() {
-    return `(${this.scope}) rider`;
-  }
+		for (var p1 of quadrants) {
+			for (var r = 0; r < 8; r++) {
+				const rad = new Point(r, r);
+
+				yield p0.add(p1.mul(new Point(m, n).mul(rad)));
+				yield p0.add(p1.mul(new Point(n, m).mul(rad)));
+			}
+		}
+	}
+
+	toString() {
+		return `(${this.scope}) rider`;
+	}
 }
 
 export function Rider(m, n) {

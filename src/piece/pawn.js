7c2b42b1708718803f960508b2017219b907c125
humanchimp <morphcham@gmail.com>
2014-08-10 17:53:09 -0700

    implement pawn move rules

--- src/piece/pawn.js
+++ src/piece/pawn.js
@@ -2,16 +2,30 @@ import { PAWN } from '../brands'
 import { Piece } from './piece'
 
 export class Pawn extends Piece {
+	
 	get brand() {
 		return PAWN;
 	}
+	
 	get unicode() {
 		return this.isWhite ? '♙' : '♟';
 	}
+	
 	get fenEncoding() {
 		return this.isWhite ? 'P' : 'p';
 	}
-	canMove(position, targetSquare) {
 
+	canMove(from, to) {
+		// pawns can only move forwards:
+		if (from.x !== to.x) {
+			return false;
+		}
+		const offset = this.isWhite ? -1 : 1;
+		const startRow = this.isWhite ? 6 : 1;
+		// pawns can move two squares on their first move.
+		if (from.y === startRow) {
+			return to.y === from.y + offset || to.y === from.y + offset * 2;
+		}
+		return to.y === from.y + offset;
 	}
 }

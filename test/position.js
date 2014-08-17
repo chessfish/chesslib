93bb1b594e82ebdf9e37d24d5108693d146ace05
humanchimp <morphcham@gmail.com>
2014-08-16 18:45:44 -0700

    test: stub tests for checkmate

--- test/position.js
+++ test/position.js
@@ -5,7 +5,7 @@ var brands = require('../lib/brands.js');
 test('position: checks', function (t) {
   var checks = [
     {
-      fen:'r2qrk2/pp1bpQ2/3p4/8/2BnP1p1/2N2P2/PPP5/2KR4 b - -',
+      fen: 'r2qrk2/pp1bpQ2/3p4/8/2BnP1p1/2N2P2/PPP5/2KR4 b - -',
       color: brands.BLACK,
     },
     {
@@ -45,5 +45,42 @@ test('not checks', function (t) {
       'it identifies the position as not check for WHITE');
     t.notOk(FEN.parse(notCheck.fen).isCheck(brands.BLACK),
       'it identifies the position as not check for BLACK');
+  });
+});
+
+test('checkmates', function (t) {
+  var checkmates = [
+    // TODO: these tests should pass. But I'm commenting them out for now.
+    // {
+    //   fen: 'r2qrk2/pp1bpQ2/3p4/8/2BnP1p1/2N2P2/PPP5/2KR4 b - -',
+    // },
+    // {
+    //   fen: 'r1b1k2r/pppp2pp/8/4N3/2B1n3/4P3/PP3qPP/RNB2K1R w kq -',
+    // },
+    {
+      fen: 'rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 4 1',
+    }
+  ];
+
+  t.plan(checkmates.length);
+
+  checkmates.forEach(function (checkmate) {
+    t.ok(FEN.parse(checkmate.fen).isCheckmate(),
+      'it identifies the position as checkmate');
+  });
+});
+
+test('not checkmates', function (t) {
+  var notCheckmates = [
+    {
+      fen: '2k2bnr/p1p2p2/1pb4p/6p1/2P5/1Q1rP1PP/PP6/3KBq2 w - -'
+    },
+  ];
+
+  t.plan(notCheckmates.length);
+
+  notCheckmates.forEach(function (notCheckmate) {
+    t.notOk(FEN.parse(notCheckmate.fen).isCheckmate(),
+      'it identifies the position as not checkmate');
   });
 });

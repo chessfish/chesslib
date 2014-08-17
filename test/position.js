5c6c8af53912cc871590ecf90569f75a0c0fdd6b
humanchimp <morphcham@gmail.com>
2014-08-16 18:41:19 -0700

    test: stub tests for not check positions

--- test/position.js
+++ test/position.js
@@ -11,12 +11,39 @@ test('position: checks', function (t) {
     {
       fen: '2k2bnr/p1p2p2/1pb4p/6p1/2P5/1Q1rP1PP/PP6/3KBq2 w - -',
       color: brands.WHITE,
+    },
+    {
+      fen: '8/4k3/pp1b2p1/3PNp2/2P1K1R1/Pr6/8/8 w - f6',
+      color: brands.WHITE,
     }
+    // TODO: add a shitload more tests.
   ];
 
   t.plan(checks.length);
 
   checks.forEach(function (check) {
-    t.ok(FEN.parse(check.fen).isCheck(check.color), 'it identifies the position as check');
+    t.ok(FEN.parse(check.fen).isCheck(check.color),
+      'it identifies the position as check');
   });
-})
+});
+
+
+test('not checks', function (t) {
+  var notChecks = [
+    {
+      fen: '3R4/pkp2ppp/1p6/2q5/8/5P1P/PP4P1/2rR3K w - -'
+    },
+    {
+      fen: 'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6'
+    }
+  ];
+
+  t.plan(notChecks.length * 2);
+
+  notChecks.forEach(function (notCheck) {
+    t.notOk(FEN.parse(notCheck.fen).isCheck(brands.WHITE),
+      'it identifies the position as not check for WHITE');
+    t.notOk(FEN.parse(notCheck.fen).isCheck(brands.BLACK),
+      'it identifies the position as not check for BLACK');
+  });
+});

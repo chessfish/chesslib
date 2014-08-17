cc5208278ed3611ffefc339a0841f2a07c065772
humanchimp <morphcham@gmail.com>
2014-08-16 22:38:23 -0700

    test: unnest tests

--- test/fischerandom.js
+++ test/fischerandom.js
@@ -5,40 +5,37 @@ var test = require('tape');
 var times = require('lodash.times');
 var FEN = require('../lib/codec/fen').FEN;
 var Chess960 = require('../lib/innovation/fischerandom.js');
-var fischerandom = Chess960.fischerandom;
-var doubleFischerandom = Chess960.doubleFischerandom;
-var ninesixty = Chess960.ninesixty;
 
 test('fischerandom', function (t) {
-  t.plan(2001);
+  t.plan(201);
 
   times(1000, function () {
-    var position = fischerandom();
+    var position = Chess960.fischerandom();
     t.ok(position.queryArray({}).length === 32,
       'fischerandom: all 32 pieces accounted-for');
   });
 
   times(1000, function () {
-    var position = doubleFischerandom();
+    var position = Chess960.doubleFischerandom();
     t.ok(position.queryArray({}).length === 32,
       'doubleFischerandom: all 32 pieces accounted-for');
   });
 
-  t.equal(fischerandom, ninesixty,
+  t.equal(Chess960.fischerandom, Chess960.ninesixty,
     'there is an alias for people who hate Bobby Fischer');
+});
 
-  test('fischerandom: finding the standard position', function(t) {
-    t.plan(1);
-    console.time('# ' + t.name);
-    var counter = 0;
-    while(true) {
-      counter += 1;
-      if (FEN.standard === FEN.stringify(fischerandom())) {
-        console.timeEnd('# ' + t.name);
-        t.ok(true, 'found it in ' + counter + ' tries');
-        console.log('# not trying that for doubleFischerandom!');
-        break;
-      }
+test('fischerandom: finding the standard position', function(t) {
+  t.plan(1);
+  console.time('# ' + t.name);
+  var counter = 0;
+  while(true) {
+    counter += 1;
+    if (FEN.standard === FEN.stringify(Chess960.fischerandom())) {
+      console.timeEnd('# ' + t.name);
+      t.ok(true, 'found it in ' + counter + ' tries');
+      console.log('# not trying that for doubleFischerandom!');
+      break;
     }
-  });
+  }
 });

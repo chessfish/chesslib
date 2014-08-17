447598fd3bf24c57f9e6278d26891b6c37c62ecd
humanchimp <morphcham@gmail.com>
2014-08-17 00:20:32 -0700

    test: add tests for pawn captures

--- test/pawn.js
+++ test/pawn.js
@@ -5,56 +5,87 @@ var test = require('tape');
 var FEN = require('../lib/codec/fen.js').FEN;
 var Point = require('../lib/point.js').Point;
 var brands = require('../lib/brands.js');
-var squareName = require('../lib/util.js').squareName;
-
-var p = FEN.standardPosition;
+var util = require('../lib/util.js');
+var squareName = util.squareName;
+var squareCoords = util.squareCoords;
 
 test('pawn movement: WHITE', function (t) {
 
-	t.plan(16);
+	t.plan(18);
 
-	p.queryArray({
+	var p1 = FEN.standardPosition;
+	var p2 = FEN.parse(
+		'rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6'
+	);
+	var p3 = FEN.parse(
+		'rnbqkbnr/pppp1ppp/8/4p3/3P4/8/PPP1PPPP/RNBQKBNR w KQkq e6'
+	);
+
+	p1.queryArray({
 		brand: brands.PAWN,
 		color: brands.WHITE,
 	}).forEach(function (pawn) {
-		var coords = p.pieceCoords(pawn);
+		var coords = p1.pieceCoords(pawn);
 
 		t.doesNotThrow(function () {
-			p.move(pawn,
+			p1.move(pawn,
 				squareName(coords.sum(new Point(0, pawn.reach))));
 		});
 
 		t.doesNotThrow(function () {
-			p.move(pawn,
+			p1.move(pawn,
 				squareName(coords.sum(new Point(0, pawn.reach * 2))));
 		});
+	});
+
+	t.doesNotThrow(function () {
+		p2.move(p2.pieceByCoords(squareCoords('e4')), 'd5'); // capture at d5
+	});
+
+	t.doesNotThrow(function () {
+		p3.move(p3.pieceByCoords(squareCoords('d4')), 'e5'); // capture at d5
 	});
 });
 
 test('pawn movement: BLACK', function (t) {
 
-	t.plan(16);
+	t.plan(18);
 
+	var p = FEN.standardPosition;
 	var whiteP = p.query({ brand: brands.PAWN, color: brands.WHITE });
 	var coords = p.pieceCoords(whiteP);
 	// move any old white pawn forward one:
-	var p2 = p.move(whiteP, squareName(coords.sum(new Point(0, whiteP.reach))));
+	var p1 = p.move(whiteP, squareName(coords.sum(new Point(0, whiteP.reach))));
+	var p2 = FEN.parse(
+		'rnbqkbnr/ppp1pppp/8/3p4/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq -'
+	);
+	var p3 = FEN.parse(
+		'rnbqkbnr/pppp1ppp/8/4p3/3P4/5N2/PPP1PPPP/RNBQKB1R b KQkq -'
+	);
 
-	p2.queryArray({
+	p1.queryArray({
 		brand: brands.PAWN,
 		color: brands.BLACK,
 	}).forEach(function (pawn) {
-		var coords = p2.pieceCoords(pawn);
+		var coords = p1.pieceCoords(pawn);
 
 		t.doesNotThrow(function () {
-			p2.move(pawn,
+			p1.move(pawn,
 				squareName(coords.sum(new Point(0, pawn.reach))));
 		});
 
 		t.doesNotThrow(function () {
-			p2.move(pawn,
+			p1.move(pawn,
 				squareName(coords.sum(new Point(0, pawn.reach * 2))));
 		});
+	});
+
+	t.doesNotThrow(function () {
+		p2.move(p2.pieceByCoords(squareCoords('d5')), 'e4'); // capture at d5
+	});
+
+	t.doesNotThrow(function () {
+		p3.move(p3.pieceByCoords(squareCoords('e5')), 'd4'); // capture at d5
 	});
 });
 

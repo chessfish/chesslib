7d2698e70932fcf1f33348948d1f05bd9b2fa189
humanchimp <morphcham@gmail.com>
2014-08-19 19:50:27 -0700

    line: basic abstraction for accumulating moves

--- src/line.js
+++ src/line.js
@@ -1,1 +1,31 @@
-export class Line {}
+import { FEN } from './fen'
+
+export class Line {
+	constructor(position=FEN.standardPosition) {
+		this.position = position;
+		this.ply = [];
+	}
+
+	addPly(ply) {
+		this.ply.push(ply);
+	}
+
+	move(move) {
+		const position = this.position.move(move);
+		this.addPly({ position, move });
+		this.position = position;
+		return this;
+	}
+
+	get plyLength() {
+		return this.ply.length;
+	}
+
+	get length() {
+		return Math.floor(this.ply.length / 2);
+	}
+}
+
+function last(arr) {
+	return arr[arr.length - 1];
+}
---
+++ test/line.js
@@ -0,0 +1,20 @@
+require('traceur/bin/traceur-runtime.js');
+require('longjohn');
+
+var test = require('tape');
+var Line = require('../lib/line').Line;
+
+test('Line class', function (t) {
+
+	t.plan(2);
+
+	var line = new Line()
+		.move('e4').move('e5')
+		.move('d4').move('exd4')
+		.move('Qxd4')
+	;
+
+	t.equal(line.plyLength, 5);
+	t.equal(line.length, 2);
+
+});

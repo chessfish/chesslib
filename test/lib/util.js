459fdf16b2e232a1991a192cb0c38301775e24b4
humanchimp <morphcham@gmail.com>
2014-08-25 21:29:04 -0700

    whitespace nits

--- test/lib/util.js
+++ test/lib/util.js
@@ -6,19 +6,19 @@ exports.getFiles = getFiles;
 exports.getFile = getFile;
 
 function getFiles(path) {
-  return (
-    fs.readdirAsync(path).
-    filter(function (filename) {
-      return filename.charAt(0) !== '.';
-    }).
-    bind({}).
-    tap(function (filenames) { this.filenames = filenames; }).
-    map(function (filename) {
-      return getFile(joinPath(path, filename));
-    })
-  );
+	return (
+		fs.readdirAsync(path).
+		filter(function (filename) {
+			return filename.charAt(0) !== '.';
+		}).
+		bind({}).
+		tap(function (filenames) { this.filenames = filenames; }).
+		map(function (filename) {
+			return getFile(joinPath(path, filename));
+		})
+	);
 }
 
 function getFile(path) {
-  return fs.readFileAsync(path, 'utf-8');
+	return fs.readFileAsync(path, 'utf-8');
 }

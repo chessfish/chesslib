0062716ab0493ddb5947d7a34b759bb2e22026ce
humanchimp <morphcham@gmail.com>
2014-08-29 20:22:45 -0700

    export more stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 require('traceur/bin/traceur-runtime.js');
-export { Position } from './position'
-export { Line } from './line'
-export { Game } from './game'
-export { FEN } from './fen'
-export { fischerandom, doubleFischerandom, ninesixty } from './fischerandom'
+export const brands = require('./brands');
+export const util = require('./util');
+export { Algebraic } from './algebraic';
+export { Board } from './board';
+export { Position } from './position';
+export { Point } from './point';
+export { Line } from './line';
+export { Game } from './game';
+export { FEN } from './fen';
+export { PGN } from './pgn';
+export { Rider } from './rider';
+export { Leaper } from './leaper';
+export { King, Queen, Rook, Bishop, Knight, Pawn } from './standard';
+export { fischerandom, doubleFischerandom, ninesixty } from './fischerandom';
+export {
+  ChessError,
+  MobilityError,
+  CheckError,
+  PromotionError,
+  ResultError,
+  AmbiguityError
+} from './error';
